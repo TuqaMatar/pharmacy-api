eef9f68b34af7002ba4c702b2aa0e12846b5d8d9
Tuqa Abu-Matar <tuqabumatar@gmail.com>
2022-05-17 20:27:44 +0300

    add id value

--- server.js
+++ server.js
@@ -2,9 +2,18 @@ const jsonServer = require('json-server');
 const server = jsonServer.create(); 
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+const express =require("express"); 
 const port = process.env.PORT || 3000; 
 
-server.use(middlewares);
-server.use(router);
+const app = express();
 
-server.listen(port);
+app.get("/", (req, res)=>{
+    res.send("Hello world")
+});
+
+app.listen( port , ()=>{
+    console.log('Example app listening on port ${3000}')
+});
+//server.use(middlewares);
+//server.use(router);
+//server.listen(port);
