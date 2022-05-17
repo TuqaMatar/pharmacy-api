dedf4f63dd85b29a2f12dbfb0b9134abb54c5373
Tuqa Abu-Matar <tuqabumatar@gmail.com>
2022-05-17 20:50:45 +0300

    delete test hello world

--- server.js
+++ server.js
@@ -2,18 +2,9 @@ const jsonServer = require('json-server');
 const server = jsonServer.create(); 
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const express =require("express"); 
 const port = process.env.PORT || 3000; 
 
-const app = express();
+server.use(middlewares);
+server.use(router);
 
-app.get("/", (req, res)=>{
-    res.send("Hello world")
-});
-
-app.listen( port , ()=>{
-    console.log('Example app listening on port ${3000}')
-});
-//server.use(middlewares);
-//server.use(router);
-//server.listen(port);
+server.listen(port);
